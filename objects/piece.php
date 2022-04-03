62c3311599c250b0222f7ceb0a890e1d79cc1394
Roy-delcom <roy@delcom.nl>
2022-04-03 14:16:30 +0200

    Validation template added for all pieces

--- objects/piece.php
+++ objects/piece.php
@@ -4,6 +4,8 @@ namespace chess;
 class piece{
     public $name;
     public $position;
+    private $_ranks = array(1,2,3,4,5,6,7,8);
+    private $_files = array('A','B','C','D','E','F','G','H');
 
     function __construct($name, $position){
         $this->name = $name;
@@ -11,16 +13,13 @@ class piece{
     }
 
     public function move($pos){
-        $ranks = array(1,2,3,4,5,6,7,8);
-        $files = array('A','B','C','D','E','F','G','H');
-
         //This is the current position before moving.
         $currentFile = $this->position[0];
-        $currentFileIndex = array_search($currentFile, $files);
+        $currentFileIndex = array_search($currentFile, $this->_files);
         $currentRank = $this->position[1];
-        $currentRankIndex = array_search($currentRank, $ranks);
-        $goToFileIndex = array_search($pos[0], $files);
-        $goToRankIndex = array_search($pos[1], $ranks);
+        $currentRankIndex = array_search($currentRank,  $this->_ranks);
+        $goToFileIndex = array_search($pos[0], $this->_files);
+        $goToRankIndex = array_search($pos[1],  $this->_ranks);
 
         if($goToFileIndex <= 0 || $goToFileIndex >= 0){
             die("This is not a space on the chessboard lmao :) ");
@@ -32,37 +31,100 @@ class piece{
 
         switch($this->name){
             case "rook":
+                $validation = $this->rookValidation($goToFileIndex,$currentFileIndex,$goToRankIndex,$currentRankIndex);
                 if($goToFileIndex == $currentFileIndex || $goToRankIndex == $currentRankIndex){
                     $this->position = $pos;    
+                }
+                else{
+                    die("Invalid rook move :(");
                 }
                 
                 break;
             case "knight":
-                //check if one goes 2 up or down and one goes 1 up or down. If thats true then valid.
-                if(($goToFileIndex == $currentFileIndex - 2 || $goToFileIndex == $currentFileIndex + 2) && ($goToRankIndex == $currentRankIndex - 1 || $goToRankIndex == $currentRankIndex + 1)){
-                    print_r("moved knight to ${$pos}");
-                }
-                else if(($goToFileIndex == $currentFileIndex - 1 || $goToFileIndex == $currentFileIndex + 1) && ($goToRankIndex == $currentRankIndex - 2 || $goToRankIndex == $currentRankIndex + 2)){
-                    print_r("moved knight to ${pos}");
+                $validation = $this->knightValidation($goToFileIndex,$currentFileIndex,$goToRankIndex,$currentRankIndex);
+                if($validation){
+                    $this->position = $pos;
                 }
                 else{
-                    print_r('invalid knight move');
+                    die("Invalid knight move :( ");
                 }
-
-                $this->position = $pos;
                 break;
             case "queen":
-                $this->position = $pos;
+                $validation = $this->queenValidation($goToFileIndex,$currentFileIndex,$goToRankIndex, $currentRankIndex);
+                if($validation){
+                   $this->position = $pos;
+                }                
                 break;
             case "king":
-                $this->position = $pos;
+                $validation = $this->kingValidation($goToFileIndex,$currentFileIndex,$goToRankIndex, $currentRankIndex);
+                if($validation){
+                   $this->position = $pos;
+                }                
                 break;
             case "pawn":
-                $this->position = $pos;
+                $validation = $this->pawnValidation($goToFileIndex,$currentFileIndex,$goToRankIndex, $currentRankIndex);
+                if($validation){
+                   $this->position = $pos;
+                }                
                 break;
             case "bishop":
-                $this->position = $pos; 
+                $validation = $this->bishopValidation($goToFileIndex,$currentFileIndex,$goToRankIndex, $currentRankIndex);
+                if($validation){
+                   $this->position = $pos;
+                }                
                 break;
         }
+    }
+
+    private function queenValidation($goToFileIndex,$currentFileIndex,$goToRankIndex, $currentRankIndex){
+        //TO-DO: Check for pieces in the way
+
+        $bishopValidation = $this->bishopValidation($goToFileIndex,$currentFileIndex,$goToRankIndex, $currentRankIndex);
+        $rookValidation = $this->rookValidation($goToFileIndex,$currentFileIndex,$goToRankIndex, $currentRankIndex);
+
+        if($rookValidation || $bishopValidation){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private function knightValidation($goToFileIndex,$currentFileIndex,$goToRankIndex,$currentRankIndex){
+        //TO-DO: Check for pieces in the way
+
+        if(($goToFileIndex == $currentFileIndex - 2 || $goToFileIndex == $currentFileIndex + 2) && ($goToRankIndex == $currentRankIndex - 1 || $goToRankIndex == $currentRankIndex + 1)){
+            return true;
+        }
+        else if(($goToFileIndex == $currentFileIndex - 1 || $goToFileIndex == $currentFileIndex + 1) && ($goToRankIndex == $currentRankIndex - 2 || $goToRankIndex == $currentRankIndex + 2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    private function rookValidation($goToFileIndex,$currentFileIndex,$goToRankIndex,$currentRankIndex){
+        //TO-DO: Check for pieces in the way
+        if($goToFileIndex == $currentFileIndex || $goToRankIndex == $currentRankIndex){
+            return true;
+        }
+        return false;
+    }
+
+    private function bishopValidation($goToFileIndex,$currentFileIndex,$goToRankIndex,$currentRankIndex){
+        //TO DO: Implement bishop validation
+        return true;
+    }
+
+    private function pawnValidation($goToFileIndex,$currentFileIndex,$goToRankIndex, $currentRankIndex){
+        //TO DO: Implement pawn validation
+        return true;
+    }
+
+    private function kingValidation($goToFileIndex,$currentFileIndex,$goToRankIndex, $currentRankIndex){
+        //TO DO: Implement king validation
+        return true;
     }
 }
