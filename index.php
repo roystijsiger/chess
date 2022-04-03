92f866810ce911e6b277da724712e5d8715848f2
Roy-delcom <roy@delcom.nl>
2022-04-03 14:30:15 +0200

    added color to the added pieces :)

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ use chess;
 $pieces = [];
 
 
-$rook = new piece("rook", "A1");
-$horse = new piece("knight", "B1");
-$pawn = new piece("pawn", "A2");
+$rook = new piece("rook", "A1", "white");
+$horse = new piece("knight", "B1", "white");
+$pawn = new piece("pawn", "A2", "white");
 
 $horse->move("D2");
 $pieces = [$rook,$horse,$pawn];
