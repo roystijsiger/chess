c2e5ed2bc335ad0e3ec9096b6cd9010f4e4a1910
Roy Stijsiger <r.stijsiger@adecon.nl>
2022-04-04 11:53:25 +0200

    styling

--- template/top.php
+++ template/top.php
@@ -10,6 +10,22 @@
                 width: 100px;
                 height: 100px;
             }
+
+            tr:nth-child(odd) > td:nth-child(odd){
+                background-color: white;
+            }
+
+            tr:nth-child(odd) > td:nth-child(even){
+                background-color: black;
+            }
+
+            tr:nth-child(even) > td:nth-child(odd){
+                background-color: black;
+            }
+
+            tr:nth-child(even) > td:nth-child(even){
+                background-color: white;
+            }
         </style>
     </head>
     <body>
