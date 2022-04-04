723b45a7d9ef2d28ee95479f31f558214b172374
Roy Stijsiger <r.stijsiger@adecon.nl>
2022-04-04 15:00:01 +0200

    add error logging

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php 
-
 namespace chess;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once './template/top.php';
 require_once './objects/piece.php';
 require_once './objects/chessBoard.php';
+
 
 $chessBoard = new ChessBoard();
 
